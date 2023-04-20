b823f733e8d128b1e10ae8b164b51d63e053fdf1
V.PRANAV <127119575+LAZER5204@users.noreply.github.com>
2023-04-20 09:49:47 +0530

    Update Courses and languages.c

--- PRACTICE PROBLEMS/Courses and languages.c
+++ PRACTICE PROBLEMS/Courses and languages.c
@@ -1,4 +1,6 @@
-/*In the new CodeChef Learn module, under the "Learn Problem Solving" section, there are two courses for each language. For eg. "Python Beginner - Part 1" and "Python Beginner - Part 2". These courses help you get started with CodeChef contests.
+/*
+CODECHEF-Q1
+In the new CodeChef Learn module, under the "Learn Problem Solving" section, there are two courses for each language. For eg. "Python Beginner - Part 1" and "Python Beginner - Part 2". These courses help you get started with CodeChef contests.
 
 Currently there are courses for 4 languages, and hence there are 8 courses in this section. But suppose there are courses for 
 �
